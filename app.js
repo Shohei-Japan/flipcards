d83478bfc4d06b9b22381ae428840eaabaaae087
Shohei Kai <kai.entertainment0304@gmail.com>
2018-08-11 01:57:00 +0900

    setting receiver

--- app.js
+++ app.js
@@ -20,13 +20,10 @@ function getType(_url) {
     return "text/plain";
 }
 
-// HTTPサーバにソケットをひもづける（WebSocket有効化）
-var io = socketio.listen(server);
-
 var server = http.createServer(function (req, res){
     var url = "public" + req.url;
     console.log(url);
-    if(fs.existsSync(url)){
+    if (fs.existsSync(url)){
         fs.readFile(url, (err, data) =>{
             if (!err){
                 res.writeHead(200, {"Content-Type": getType(url)});
@@ -39,17 +36,17 @@ console.log("サーバーに繋がりましたよ！");
 
 //  HTTPサーバにソケットを紐付ける（WebSocket有効化）
 var io = socketio.listen(server);
- 
+
 var cards = [];
 const marks = ['s', 'c', 'h', 'd']; // スペード、クローバー、ハート、ダイヤ
 var flipedCard;
 
 // connectionイベント・データを受信する
 io.sockets.on('connection', function(socket) {
-    console.log("connectionした");
+    // console.log("connectionした");
     // client_to_serverイベント・データを受信する
     socket.on('client_to_server_onload', function() {
-        console.log("readyした");
+        // console.log("readyした");
         cards = [];
         for(let i = 0; i < marks.length; i++){
             for(let j = 1; j < 14; j++){ // s01 〜 d13まで生成
@@ -60,27 +57,61 @@ io.sockets.on('connection', function(socket) {
                 }
                 cards.push(card); // 連想配列
                 // console.log("cardsにpushしたよ"+i+"回目");
-                // console.log(card);
-                // console.log(cards);
             }
-            console.log("cardsに全部pushしたよ"+i+"回目");
-            // console.log(cards);
+            // console.log("cardsに全部pushしたよ"+i+"回目");
         }
-        // console.log(cards);
-        console.log("shuffleに送るよ")
+        // console.log("shuffleに送るよ")
         shuffle(cards);
     });
-    // console.log(cards);
     // server_to_clientイベント・データを送信する
+
+    // clickを受け取る
+    socket.on('client_to_server_click', function(srcJSON){
+        console.log("click情報受け取った");
+        var srcParse = JSON.parse(srcJSON);
+        // var src = JSON.parse(srcJSON);
+        // console.log('srcは'+src);
+        // console.log("srcParse.indexは" + srcParse.index);
+        var firstCard = src.value;
+        console.log('firstCardは'+firstCard);
+        var num = firstCard.number; // 2
+        console.log('numは'+num);
+        firstCard.className = 'card open';
+
+        var flipedCard;
+        if(flipedCard == null){
+            console.log('1');
+            flipedCard = firstCard;
+            firstCard.backgroundImage = "url(" + firstCard.index + ")";
+            console.log("flipedCardは"+flipedCard);
+            return;
+        }
+        // console.log(flipedCard.id, e.srcElement.id);
+        // 同じカードを２回クリックしても反応しない
+        if(flipedCard.id == firstCard.id){
+            return;
+        }
+        if(flipedCard.number == num){ // ２枚のカードの数字が同じ場合
+            console.log('2');
+            flipedCard.backgroundImage = "url(" + e.srcElement.index + ")";
+            flipedCard = null;
+        } else { // カードの数字が違う場合
+            console.log('3');
+            e.srcElement.style.backgroundImage = "url(" + e.srcElement.index + ")";
+            setTimeout(function () {
+                firstCard.style.backgroundImage = "url(trump/png/z01.png)";
+                flipedCard.style.backgroundImage = "url(trump/png/z01.png)";
+                flipedCard = null;
+            }, 1000);
+        }
+    });
 });
-
-
 
 // 生成したカードをシャッフル
 function shuffle(cards){
-    console.log("shuffleです。cards受け取りました。")
+    console.log("shuffleです。cards受け取りました。");
     console.log(cards.length);
-    console.log("shuffleするよ")
+    console.log("shuffleするよ");
     // console.log(cards);
     for(var i = cards.length - 1; i >= 0; i--){
         var r = Math.floor(Math.random() * (i + 1));
@@ -92,35 +123,4 @@ function shuffle(cards){
     console.log("cards情報送るよ");
     io.sockets.emit('server_to_client_draw', {value : cards});
     console.log("cards情報送った！");
-}
-
-
-function click(e){
-    let firstCard = e.srcElement;
-    console.log(firstCard);
-    let num = firstCard.number
-    firstCard.className = 'card open';
-    
-    if(flipedCard == null){
-        flipedCard = firstCard;
-        firstCard.style.backgroundImage = "url(" + firstCard.index + ")";
-        return;
-    }
-    // console.log(flipedCard.id, e.srcElement.id);
-    // 同じカードを２回クリックしても反応しない
-    if(flipedCard.id == e.srcElement.id){
-        return;
-    }
-    if(flipedCard.number == num){ // ２枚のカードの数字が同じ場合
-        // console.log(e.srcElement);
-        e.srcElement.style.backgroundImage = "url(" + e.srcElement.index + ")";
-        flipedCard = null;
-    } else {
-        e.srcElement.style.backgroundImage = "url(" + e.srcElement.index + ")";
-        setTimeout(function () {
-            firstCard.style.backgroundImage = "url(trump/png/z01.png)";
-            flipedCard.style.backgroundImage = "url(trump/png/z01.png)";
-            flipedCard = null;
-        }, 1000);
-    }
 }
