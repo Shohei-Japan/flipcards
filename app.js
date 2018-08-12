789546880c643fd5a9c2d1247bfe0bb37dab24c7
Shohei Kai <kai.entertainment0304@gmail.com>
2018-08-12 23:22:22 +0900

    message info

--- app.js
+++ app.js
@@ -53,23 +53,26 @@ io.sockets.on('connection', function(socket) {
         room = data.value;
         socket.join(room);
     });
-    // S05. client_to_serverイベント・データを受信する
-    socket.on('client_to_server', function(data) {
-        // S06. server_to_clientイベント・データを送信する
+
+    // メッセージ
+    socket.on('client_to_server_message', function(data) {
         io.to(room).emit('server_to_client', {value : data.value});
     });
-    // S07. client_to_server_broadcastイベント・データを受信し、送信元以外に送信する
+
+    // 入室情報
     socket.on('client_to_server_broadcast', function(data) {
         socket.broadcast.to(room).emit('server_to_client', {value : data.value});
     });
-    // S08. client_to_server_personalイベント・データを受信し、送信元のみに送信する
+
+    // 自分のみに送信
     socket.on('client_to_server_personal', function(data) {
         var id = socket.id;
         name = data.value;
         var personalMessage = "あなたは、" + name + "さんとして入室しました。"
         io.to(id).emit('server_to_client', {value : personalMessage});
     });
-    // S09. dicconnectイベントを受信し、退出メッセージを送信する
+
+    // 退出情報
     socket.on('disconnect', function() {
         if (name == '') {
             console.log("未入室のまま、どこかへ去っていきました。");
