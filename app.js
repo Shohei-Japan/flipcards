5121295519af2a5129b3fc3083da79a3aa8a6906
Shohei Kai <kai.entertainment0304@gmail.com>
2018-08-21 23:27:33 +0900

    edit

--- app.js
+++ app.js
@@ -4,6 +4,38 @@ var socketio = require('socket.io');
 var fs = require('fs');
 
 function getType(_url) {
+    var types = {
+        ".html": "text/html",
+        ".css": "text/css",
+        ".js": "text/javascript",
+        ".png": "image/png",
+        ".gif": "image/gif",
+        ".svg": "svg+xml"
+    }
+    for (var key in types) {
+        if (_url.endsWith(key)) {
+            return types[key];
+        }
+    }
+    return "text/plain";
+}
+
+var server = http.createServer(function (req, res){
+    var url = "public" + req.url;
+    // console.log(url);
+    if (fs.existsSync(url)){
+        fs.readFile(url, (err, data) =>{
+            if (!err){
+                res.writeHead(200, {"Content-Type": getType(url)});
+                res.end(data);
+            }
+        });
+    }
+}).listen(3000);
+console.log("サーバーに繋がりましたよ！");
+
+//  HTTPサーバにソケットを紐付ける（WebSocket有効化）
+var io = socketio.listen(server);
 
 const marks = ['s', 'c', 'h', 'd']; // スペード、クローバー、ハート、ダイヤ
 var allCards = []; // rooms = [[cards],[cards],[cards]];
